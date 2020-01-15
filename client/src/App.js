1c8b68631a282f5f95183ec7b30faf111c98c340
Dominik <dominik_p@wp.pl>
2020-01-15 23:10:59 +0100

    Reset

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import MainNavigation from './components/MainNavigation/MainNavigation';
+import Events from './pages/Events/Events';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <MainNavigation />
-      </div>
+      <Fragment>
+        
+      </Fragment>
     )
   }
 }
